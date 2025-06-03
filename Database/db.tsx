2639b0a7ea945b0506d7f5fac979e5b5c68d52b5
Ivan Diaz <ivan.diaz@datadoghq.com>
2025-06-02 23:15:05 -0600

    Clean data

--- Database/db.tsx
+++ Database/db.tsx
@@ -1,12 +1,13 @@
 // db.js
 import postgres from 'postgres'
 
+const ENV = process.env;
 const sql = postgres('postgres://postgres:Fantasmas24@localhost:5433/FinanceAppDatabase', {
     host                 : 'localhost',            // Postgres ip address[s] or domain name[s]
     port                 : 5433,          // Postgres server port[s]
     database             : 'FinanceAppDatabase',            // Name of database to connect to
-    username             : 'postgres',            // Username of database user
-    password             : 'Fantasmas24'            // Password of database user
+    username             : ENV.DATABASE_USERNAME,            // Username of database user
+    password             : ENV.DATABASE_PASSWORD,            // Password of database user
   })
 
 export default sql
