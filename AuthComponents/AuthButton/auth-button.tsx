12ed685173ce29fc547f6e639d9f8b86dc7ae711
Ivan Diaz <ivan.diaz@datadoghq.com>
2025-06-30 11:22:52 -0600

    fix initial login re-routing

--- AuthComponents/AuthButton/auth-button.tsx
+++ AuthComponents/AuthButton/auth-button.tsx
@@ -2,11 +2,10 @@
 
 import MyButton from "@/app/Components/Button/Button";
 
-
 export function AuthButton() {
   return (
     <div className="authentication">
-      <MyButton text="Sign in" url="/auth/login" size="small">
+      <MyButton text="Sign in" url="/routes/home" size="small">
       </MyButton>
       <MyButton text="Sign up" url="/auth/sign-up" size="small" variant="outlined-button">
       </MyButton>
