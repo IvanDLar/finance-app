7c0cd69c34184f5a6901f1c108db5e18547d5a97
Iván Díaz <65410762+IvanDLar@users.noreply.github.com>
2025-11-16 05:49:47 +0000

    feat(transactions-list): Group and sort transactions by date

--- app/Components/TransactionsList/TransactionsList.tsx
+++ app/Components/TransactionsList/TransactionsList.tsx
@@ -1,26 +1,67 @@
 import TransactionWidget from '../TransactionWidget/TransactionWidget';
 import { Transaction } from '@/app/Types/Transactions';
+import moment from 'moment';
+import { useEffect, useState } from 'react';
 
-type TransactionsTableProps = {
-  data: Transaction[];
+type TransactionsT = Transaction[];
+type TransactionsPerDayT = [string, TransactionsT][];
+type TransactionsPerDayMapT = {
+  [key: string]: TransactionsT;
 };
 
-const TransactionsList = ({ data }: TransactionsTableProps) => {
-  // const groupTransactionsPerDate = () => {
+// TODO: Tiddy up a bit the logic here and give the date a style.
+const TransactionsList = ({ data }: { data: TransactionsT }) => {
+  const [transactionsPerDay, setTransactionsPerDay] =
+    useState<TransactionsPerDayT>([]);
+  const groupTransactionsPerDate = () => {
+    if (!data || !data.length) setTransactionsPerDay([]);
+    let transactionsPerDayMap: TransactionsPerDayMapT = {};
 
-  // };
+    for (let transaction of data) {
+      let momentDate = moment(transaction.date);
+      let momentFormatedDate = momentDate.format('L');
+
+      // We know that the dates are ordered, so we can assume and iterate
+      // through them and knoe 0 -> n are in order, we just need to group them by date.
+      if (transactionsPerDayMap[momentFormatedDate]) {
+        const existingArray = transactionsPerDayMap[momentFormatedDate];
+        existingArray.push(transaction);
+        transactionsPerDayMap[momentFormatedDate] = existingArray;
+      } else {
+        transactionsPerDayMap[momentFormatedDate] = [transaction];
+      }
+    }
+
+    // Turn the map into an array and order it by date
+    let ascendingOrderedObject: TransactionsPerDayT = Object.entries(
+      transactionsPerDayMap,
+    ).sort((a, b) => moment(b[0]).diff(moment(a[0])));
+    setTransactionsPerDay(ascendingOrderedObject);
+  };
+
+  useEffect(() => {
+    groupTransactionsPerDate();
+  }, [data]);
 
   return (
     <div>
-      {data.map((transaction) => {
+      {transactionsPerDay.map((dateTransactions) => {
+        const [date, datesArray] = dateTransactions;
         return (
-          <TransactionWidget
-            name={transaction.name}
-            amount={transaction.amount}
-            isIncome={transaction.is_income}
-            category={transaction.category}
-            key={transaction.id}
-          />
+          <>
+            <div key={date}>{date}</div>
+            {datesArray.map((transaction) => {
+              return (
+                <TransactionWidget
+                  name={transaction.name}
+                  amount={transaction.amount}
+                  isIncome={transaction.is_income}
+                  category={transaction.category}
+                  key={transaction.id}
+                />
+              );
+            })}
+          </>
         );
       })}
     </div>
